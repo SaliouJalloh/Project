ae1cd4d2b72577f82f0fb3b64768421b2546ae23
DIALLO Mamadou_Saliou <mamadou.diallo.6@etu.u-bordeaux.fr>
2019-11-17 00:15:55 +0100

    complement td1

--- TD1/app/src/main/java/com/example/td1/MainActivity.java
+++ TD1/app/src/main/java/com/example/td1/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private Bitmap bitmap;
     private Button button;
     private int width;
-    private  int height;
+    private int height;
+    private int tmp_color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,35 +39,70 @@ public class MainActivity extends AppCompatActivity {
 
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inMutable = true;
+
+            // convertToMutable(img_bp);
             bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.fruit,options);
 
-            taille.setText( bitmap.getWidth() + "*" + bitmap.getHeight());
+            taille.setText( "Taille : " + bitmap.getWidth() + "*" + bitmap.getHeight());
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    toGray(bitmap);
+                    // colorize(img_bp);
+                    //colorize_Without_Red(img_bp);
+                    img.setImageBitmap(bitmap);
+
+                }
+            });
 
             width = bitmap.getWidth();
             height = bitmap.getHeight();
 
             for(int x = 0; x < width; ++x){
-                for (int y = 0; y < height; ++y){
-
+                for (int y = height/2; y < height; ++y){
+                    bitmap.setPixel(x,y, Color.BLACK);
                 }
             }
     }
 
-    public void toGray(Bitmap bmp){
+    public Bitmap toGray(Bitmap bmp){
+
+        width = bmp.getWidth();
+        height = bmp.getHeight();
+        int R, G, B, R_G_B;
+
+        for(int x = 0; x < width; ++x){
+            for (int y = 0; y < height; ++y){
+                tmp_color = bmp.getPixel(x,y);
+                R = 0; //  0.3R + 0.59G + 0.11B.
+                G = 0;
+                B = 0;
+                R_G_B = (R+G+B)/3;
+
+                bmp.setPixel(x,y,tmp_color);
+            }
+        }
+        return bmp;
+    }
+
+    public Bitmap toGrays(Bitmap bmp){
+
+        width = bmp.getWidth();
+        height = bmp.getHeight();
+        int[] pixels = new int[width * height];
+        bmp.getPixels(pixels, 0, 0, 0, 0, width, height);
 
         for(int x = 0; x < width; ++x){
             for (int y = 0; y < height; ++y){
 
+
+
+
+                bitmap.getPixels(pixels,0,0,0,0,0,0);
             }
         }
+        return bmp;
     }
 
-    public void toGrays(Bitmap bmp){
-
-        for(int x = 0; x < width; ++x){
-            for (int y = 0; y < height; ++y){
-
-            }
-        }
-    }
 }
