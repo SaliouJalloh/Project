c2eaa3a41535d2068ec9a4343d0c4f72683a5dce
Mamadou Saliou Diallo <mdiallo004@emi.u-bordeaux1.fr>
2019-11-16 17:17:18 +0100

    petit ajout de togray

--- TD1/app/src/main/java/com/example/td1/MainActivity.java
+++ TD1/app/src/main/java/com/example/td1/MainActivity.java
@@ -12,32 +12,59 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+    private TextView text;
+    private TextView taille;
+    private ImageView img;
+    private Bitmap bitmap;
+    private Button button;
+    private int width;
+    private  int height;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        private TextView text;
-        private ImageView img;
-        private Bitmap bitmap;
-        private Button button;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
 
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
             // Ajout des widgets
 
-            text = findViewById(R.id.idText);
-            img = findViewById(R.id.idImage);
-            button = findViewById(R.id.idButton);
+            text = findViewById(R.id.idtext);
+            taille = findViewById(R.id.idtaille);
+            img = findViewById(R.id.idimage);
+            button = findViewById(R.id.idbutton);
 
             // Convertion de l'image
 
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inMutable = true;
-            bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.img,options);
+            bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.fruit,options);
 
-            text.setText("Width : " + bitmap.getWidth() + "\n");
-            text.setText("Height : " + bitmap.getHeight() + "\n");
+            taille.setText( bitmap.getWidth() + "*" + bitmap.getHeight());
 
+            width = bitmap.getWidth();
+            height = bitmap.getHeight();
+
+            for(int x = 0; x < width; ++x){
+                for (int y = 0; y < height; ++y){
+
+                }
+            }
+    }
+
+    public void toGray(Bitmap bmp){
+
+        for(int x = 0; x < width; ++x){
+            for (int y = 0; y < height; ++y){
+
+            }
+        }
+    }
+
+    public void toGrays(Bitmap bmp){
+
+        for(int x = 0; x < width; ++x){
+            for (int y = 0; y < height; ++y){
+
+            }
+        }
     }
 }
