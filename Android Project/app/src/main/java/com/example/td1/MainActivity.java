411706d146501de2aa1758d46dcc6468c0392039
Tanguy_BERASATEGUY <tanguy.berasateguy@etu.u-bordeaux.fr>
2020-02-07 14:50:14 +0100

    Convolution

--- Android Project/app/src/main/java/com/example/td1/MainActivity.java
+++ Android Project/app/src/main/java/com/example/td1/MainActivity.java
@@ -841,5 +841,30 @@ public class MainActivity extends AppCompatActivity {
     }
     */
 
+    protected void convolution(Bitmap bmp){
+        int h = bmp.getHeight();
+        int w = bmp.getWidth();
+        int pixels[] = new int[h*w];
+        int red=0;
+        int green=0;
+        int blue=0;
+        bmp.getPixels(pixels, 0, w, 0, 0, w, h);
+        for(int i=1; i<w-1; i++){
+            for(int j=1; j<h-1; j++){
+                red = (Color.red(pixels[i-1+(j-1)*w]) + Color.red(pixels[i+(j-1)*w]) + Color.red(pixels[i+1+(j-1)*w]) + Color.red(pixels[i-1+j*w]) + Color.red(pixels[i+j*w]) + Color.red(pixels[i+1+j*w]) + Color.red(pixels[i-1+(j+1)*w]) + Color.red(pixels[i+(j+1)*w]) + Color.red(pixels[i+1+(j+1)*w]))/9;
+                green = (Color.green(pixels[i-1+(j-1)*w]) + Color.green(pixels[i+(j-1)*w]) + Color.green(pixels[i+1+(j-1)*w]) + Color.green(pixels[i-1+j*w]) + Color.green(pixels[i+j*w]) + Color.green(pixels[i+1+j*w]) + Color.green(pixels[i-1+(j+1)*w]) + Color.green(pixels[i+(j+1)*w]) + Color.green(pixels[i+1+(j+1)*w]))/9;
+                blue = (Color.blue(pixels[i-1+(j-1)*w]) + Color.blue(pixels[i+(j-1)*w]) + Color.blue(pixels[i+1+(j-1)*w]) + Color.blue(pixels[i-1+j*w]) + Color.blue(pixels[i+j*w]) + Color.blue(pixels[i+1+j*w]) + Color.blue(pixels[i-1+(j+1)*w]) + Color.blue(pixels[i+(j+1)*w]) + Color.blue(pixels[i+1+(j+1)*w]))/9;
+                pixels[i+j*w] = Color.rgb(red,green,blue);
+                /*
+                red = 0;
+                green = 0;
+                blue = 0;
+                */
+            }
+        }
+        bmp.setPixels(pixels, 0, w, 0, 0, w, h);
+
+    }
+
 
 }
