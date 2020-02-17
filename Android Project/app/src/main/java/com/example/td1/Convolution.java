630c7e5b752e8eb948f9f05df045e711472fdde2
Tanguy_BERASATEGUY <tanguy.berasateguy@etu.u-bordeaux.fr>
2020-02-17 15:36:27 +0100

    Deplacement fonctions Convolution + ajout taille convolutionMoy(n)

--- Android Project/app/src/main/java/com/example/td1/Convolution.java
+++ Android Project/app/src/main/java/com/example/td1/Convolution.java
@@ -5,58 +5,71 @@ import android.graphics.Color;
 
 public class Convolution {
 
-
-    protected void convolution(Bitmap bmp){
-        int h = bmp.getHeight();
-        int w = bmp.getWidth();
-        int pixels[] = new int[h*w];
-        int red=0;
-        int green=0;
-        int blue=0;
-        bmp.getPixels(pixels, 0, w, 0, 0, w, h);
-        //for(int k=0; k<10; k++) {
-        for (int i = 3; i < w - 3; i++) {
-            for (int j = 3; j < h - 3; j++) {
-                red = (Color.red(pixels[i - 1 + (j - 1) * w]) + Color.red(pixels[i + (j - 1) * w]) + Color.red(pixels[i + 1 + (j - 1) * w]) + Color.red(pixels[i - 1 + j * w]) + Color.red(pixels[i + j * w]) + Color.red(pixels[i + 1 + j * w]) + Color.red(pixels[i - 1 + (j + 1) * w]) + Color.red(pixels[i + (j + 1) * w]) + Color.red(pixels[i + 1 + (j + 1) * w])) / 9;
-                green = (Color.green(pixels[i - 1 + (j - 1) * w]) + Color.green(pixels[i + (j - 1) * w]) + Color.green(pixels[i + 1 + (j - 1) * w]) + Color.green(pixels[i - 1 + j * w]) + Color.green(pixels[i + j * w]) + Color.green(pixels[i + 1 + j * w]) + Color.green(pixels[i - 1 + (j + 1) * w]) + Color.green(pixels[i + (j + 1) * w]) + Color.green(pixels[i + 1 + (j + 1) * w])) / 9;
-                blue = (Color.blue(pixels[i - 1 + (j - 1) * w]) + Color.blue(pixels[i + (j - 1) * w]) + Color.blue(pixels[i + 1 + (j - 1) * w]) + Color.blue(pixels[i - 1 + j * w]) + Color.blue(pixels[i + j * w]) + Color.blue(pixels[i + 1 + j * w]) + Color.blue(pixels[i - 1 + (j + 1) * w]) + Color.blue(pixels[i + (j + 1) * w]) + Color.blue(pixels[i + 1 + (j + 1) * w])) / 9;
-                pixels[i + j * w] = Color.rgb(red, green, blue);
-                /*
-                red = 0;
-                green = 0;
-                blue = 0;
-                */
-            }
-        }
-        // }
-        bmp.setPixels(pixels, 0, w, 0, 0, w, h);
-
-    }
-
-    protected void convolution5(Bitmap bmp){
-        int h = bmp.getHeight();
-        int w = bmp.getWidth();
-        int pixels[] = new int[h*w];
-        int red=0;
-        int green=0;
-        int blue=0;
-        bmp.getPixels(pixels, 0, w, 0, 0, w, h);
-        for(int i=5; i<w-5; i++) {
-            for (int j = 5; j < h - 5; j++) {
-                for (int k = i - 2; k <= i + 2; k++) {
-                    for (int l = j - 2; l <= j + 2; l++) {
+    protected static void convolutionMoy(Bitmap bmp ,int n){
+        int h = bmp.getHeight(), w = bmp.getWidth();    //Get the size of the bmp
+        int pixels[] = new int[h*w];                    //Create a table to contain the pixels of the bmp
+        bmp.getPixels(pixels, 0, w, 0, 0, w, h);    //Get the pixels of the bmp in the table
+        int red=0, green=0, blue=0;                     //Create args to get the pixel's color values
+        int filterSize = n, halfSize = filterSize/2, filterSlots = filterSize*filterSize; //Size of the filter
+        for(int i=halfSize; i<w-halfSize; i++) {                 //For each pixels apply the filter
+            for (int j = halfSize; j < h - halfSize; j++) {
+                for (int k = i - halfSize; k <= i + halfSize; k++) {
+                    for (int l = j - halfSize; l <= j + halfSize; l++) {
                         red += Color.red((pixels[k + l * w]));
                         green += Color.green((pixels[k + l * w]));
                         blue += Color.blue((pixels[k + l * w]));
                     }
                 }
-                red/=25; green/=25; blue/=25;
+                red/=filterSlots; green/=filterSlots; blue/=filterSlots;
                 pixels[i + j * w] = Color.rgb(red, green, blue);
                 red=0;
                 green=0;
                 blue=0;
             }
         }
+        bmp.setPixels(pixels, 0, w, 0, 0, w, h);    //Set the pixels in the bmp
+    }
+
+    /*
+     * TODO:
+     *  - add "N" parameter to the method
+     *  - let the user choose the size of the filter in a list of predefined sizes (n%2 != 0)
+     */
+    protected static void convolutionGaus(Bitmap bmp /*, int n*/){
+        int h = bmp.getHeight(), w = bmp.getWidth();        //Get the size of the bmp
+        int pixels[] = new int[h*w];                        //Create a table to contain the pixels of the bmp
+        bmp.getPixels(pixels, 0, w,0,0, w, h);  //Get the pixels of the bmp in the table
+        int red = 0, green = 0, blue = 0;                   //Create args to get the pixel's color values
+        //size : n/2 + 1
+        int halfSize = 13, filterSize = 2*halfSize+1, filterSlots = filterSize*filterSize;   //Size of the filter
+        int filter[] = new int[filterSlots];        //Create a table to contain the filter
+        int sumFilter = 0;                          //Sum of values in the filter -> values of pixels to divide by to be in [0;255]
+        double ecartType = halfSize/Math.sqrt(-2*Math.log(0.05));   //Need to calculate filter's values
+        for(int i=0; i<filterSize; i++){            //Calculate the values of the filter
+            for(int j=0; j<filterSize; j++){
+                int val = (int) ((Math.exp(-((i*i+j*j)/2*ecartType*ecartType)))*10000);
+                filter[i+j*filterSize] = (10001-val);
+                sumFilter+=val;
+            }
+        }
+
+        for(int i=halfSize; i<w-halfSize; i++) {                 //For each pixels apply the filter
+            for (int j = halfSize; j < h - halfSize; j++) {
+                int slot=0;
+                for (int k = i - halfSize; k <= i + halfSize; k++) {
+                    for (int l = j - halfSize; l <= j + halfSize; l++) {
+                        System.out.println("TEEEEEST    red "+Color.green(pixels[k+l*w]));
+                        System.out.println("TEEEEEST    filter "+filter[slot]/sumFilter);
+                        //red = Color.red(pixels[k+l*w])*filter[slot]/sumFilter;
+                        green = Color.green(pixels[k+l*w])*filter[slot]/sumFilter;
+                        blue = Color.blue(pixels[k+l*w])*filter[slot]/sumFilter;
+                        pixels[k+l*w] = Color.rgb(red,green,blue);
+                        slot++;
+                    }
+                }
+            }
+        }
+        bmp.setPixels(pixels, 0, w, 0, 0, w, h);    //Set the pixels in the bmp
     }
 
 
--- Android Project/app/src/main/java/com/example/td1/MainActivity.java
+++ Android Project/app/src/main/java/com/example/td1/MainActivity.java
@@ -282,17 +282,21 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this,"down contrast selected",Toast.LENGTH_LONG).show();
                 return true;
             case R.id.menu_to_convolution:
-                convolutionMoy(bitmap2);
-                img.setImageBitmap(bitmap2);
                 Toast.makeText(this,"Convolution Moy selected",Toast.LENGTH_LONG).show();
-                return true;
                 /*
+                 * TODO:
+                 *  - let the user choose the size of the filter in a list of predefined sizes (n%2 != 0)
+                 */
+                Convolution.convolutionMoy(bitmap,19);
+                img.setImageBitmap(bitmap);
+                return true;
+                ///*
             case R.id.menu_to_convolutionGaus:
-                convolutionGaus(bitmap2);
                 Toast.makeText(this,"Convolution Moy selected",Toast.LENGTH_LONG).show();
+                Convolution.convolutionGaus(bitmap2);
                 img.setImageBitmap(bitmap2);
 
-                 */
+                 //*/
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -645,81 +649,6 @@ public class MainActivity extends AppCompatActivity {
         tab[1] = max;
         return tab;
     }
-
-
-    /*
-     * TODO:
-     *  - add "N" parameter to the method
-     *  - let the user choose the size of the filter in a list of predefined sizes (n%2 != 0)
-     */
-    protected void convolutionMoy(Bitmap bmp /*,int n*/){
-        int h = bmp.getHeight(), w = bmp.getWidth();    //Get the size of the bmp
-        int pixels[] = new int[h*w];                    //Create a table to contain the pixels of the bmp
-        bmp.getPixels(pixels, 0, w, 0, 0, w, h);    //Get the pixels of the bmp in the table
-        int red=0, green=0, blue=0;                     //Create args to get the pixel's color values
-        int filterSize = 9, halfSize = filterSize/2, filterSlots = filterSize*filterSize; //Size of the filter
-        for(int i=halfSize; i<w-halfSize; i++) {                 //For each pixels apply the filter
-            for (int j = halfSize; j < h - halfSize; j++) {
-                for (int k = i - halfSize; k <= i + halfSize; k++) {
-                    for (int l = j - halfSize; l <= j + halfSize; l++) {
-                        red += Color.red((pixels[k + l * w]));
-                        green += Color.green((pixels[k + l * w]));
-                        blue += Color.blue((pixels[k + l * w]));
-                    }
-                }
-                red/=filterSlots; green/=filterSlots; blue/=filterSlots;
-                pixels[i + j * w] = Color.rgb(red, green, blue);
-                red=0;
-                green=0;
-                blue=0;
-            }
-        }
-        bmp.setPixels(pixels, 0, w, 0, 0, w, h);    //Set the pixels in the bmp
-    }
-
-    /*
-     * TODO:
-     *  - add "N" parameter to the method
-     *  - let the user choose the size of the filter in a list of predefined sizes (n%2 != 0)
-     */
-    protected void convolutionGaus(Bitmap bmp /*, int n*/){
-        int h = bmp.getHeight(), w = bmp.getWidth();        //Get the size of the bmp
-        int pixels[] = new int[h*w];                        //Create a table to contain the pixels of the bmp
-        bmp.getPixels(pixels, 0, w,0,0, w, h);  //Get the pixels of the bmp in the table
-        int red = 0, green = 0, blue = 0;                   //Create args to get the pixel's color values
-        //size : n/2 + 1
-        int halfSize = 13, filterSize = 2*halfSize+1, filterSlots = filterSize*filterSize;   //Size of the filter
-        int filter[] = new int[filterSlots];        //Create a table to contain the filter
-        int sumFilter = 0;                          //Sum of values in the filter -> values of pixels to divide by to be in [0;255]
-        double ecartType = halfSize/Math.sqrt(-2*Math.log(0.05));   //Need to calculate filter's values
-        for(int i=0; i<filterSize; i++){            //Calculate the values of the filter
-            for(int j=0; j<filterSize; j++){
-                int val = (int) ((Math.exp(-((i*i+j*j)/2*ecartType*ecartType)))*10000);
-                filter[i+j*filterSize] = (10001-val);
-                sumFilter+=val;
-            }
-        }
-
-        for(int i=halfSize; i<w-halfSize; i++) {                 //For each pixels apply the filter
-            for (int j = halfSize; j < h - halfSize; j++) {
-                int slot=0;
-                for (int k = i - halfSize; k <= i + halfSize; k++) {
-                    for (int l = j - halfSize; l <= j + halfSize; l++) {
-                        System.out.println("TEEEEEST    red "+Color.green(pixels[k+l*w]));
-                        System.out.println("TEEEEEST    filter "+filter[slot]/sumFilter);
-                        //red = Color.red(pixels[k+l*w])*filter[slot]/sumFilter;
-                        green = Color.green(pixels[k+l*w])*filter[slot]/sumFilter;
-                        blue = Color.blue(pixels[k+l*w])*filter[slot]/sumFilter;
-                        pixels[k+l*w] = Color.rgb(red,green,blue);
-                        slot++;
-                    }
-                }
-            }
-        }
-        bmp.setPixels(pixels, 0, w, 0, 0, w, h);    //Set the pixels in the bmp
-    }
-
-
 
 /**************************************************************************************************************************************/
                  /****************************************************************************************/
