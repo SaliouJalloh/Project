45f056b3edd14aeb61dc3ff60bb83e44d515dcb1
Mamadou Saliou Diallo <mdiallo004@emi.u-bordeaux1.fr>
2020-02-20 20:51:50 +0100

    clean du main

--- Android Project/app/src/main/java/com/example/td1/MainActivity.java
+++ Android Project/app/src/main/java/com/example/td1/MainActivity.java
@@ -89,11 +89,7 @@ public class MainActivity extends AppCompatActivity /*implements AdapterView.OnI
 
         createOnClickButton();
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.SpinnerN, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
+
     }
 
     private void createOnClickButton(){
